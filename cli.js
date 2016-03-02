ff7457c825f62beb20089d8276fca56933ed04bc
Jeroen Engels <jfm.engels@gmail.com>
2016-03-02 23:45:23 +0100

    Adding contributors to config file

--- cli.js
+++ cli.js
@@ -7,20 +7,23 @@ var assign = require('lodash.assign');
 
 var generate = require('./lib/generate');
 var markdown = require('./lib/markdown');
-var getUserInfo = require('./lib/github');
+var updateContributors = require('./lib/contributors');
 
 var cwd = process.cwd();
 var defaultRCFile = path.join(cwd, '.all-contributorsrc');
 
 var argv = require('yargs')
+  .help('help')
+  .alias('h', 'help')
   .command('generate', 'Generate the list of contributors')
   .usage('Usage: $0 generate')
   .command('add', 'add a new contributor')
   .usage('Usage: $0 add <username> <contribution>')
   .demand(2)
-  .default('config', defaultRCFile)
   .default('file', 'README.md')
   .default('contributorsPerLine', 7)
+  .default('contributors', [])
+  .default('config', defaultRCFile)
   .config('config', function(configPath) {
     try {
       return JSON.parse(fs.readFileSync(configPath, 'utf-8'));
@@ -31,7 +34,6 @@ var argv = require('yargs')
       }
     }
   })
-  .help('help')
   .argv;
 
 argv.file = path.join(cwd, argv.file);
@@ -52,19 +54,19 @@ function onError(error) {
   }
 }
 
-if (argv[0] === 'generate') {
+var command = argv._[0];
+
+if (command === 'generate') {
   startGeneration(argv, onError);
-} else if (argv[0] === 'add') {
-  // Fetch user
-  argv.username = argv._[1];
-  argv.contributions = argv._[2].split(',');
-  getUserInfo(argv.username, function(error, user) {
+} else if (command === 'add') {
+  var username = argv._[1];
+  var contributions = argv._[2];
+  // Add/update contributor and save him to the config file
+  updateContributors(argv, username, contributions, function(error, contributors) {
     if (error) {
-      return console.error(error);
+      return onError(error);
     }
-    // TODO
-    // Add him to the contributors
-    // Save rc file with updated contributors key
+    argv.contributors = contributors;
     startGeneration(argv, onError);
   });
 }

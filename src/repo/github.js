a13d060064c30d3c467b81908dbef7b493a87846
Joao Marques <jgmarques@kpmg.com>
2018-02-19 19:49:12 +0000

    refactor(repo): declare the functions outside the module.exports
    this allows for future call of these functions recursively if necessary

--- src/repo/github.js
+++ src/repo/github.js
@@ -38,48 +38,51 @@ function getContributorsPage(url) {
     })
 }
 
-module.exports = {
-	getUserInfo(username, hostname) {
-  	/* eslint-disable complexity */
-		if (!hostname) {
-			hostname = 'https://github.com';
-		}
-
-		const root = hostname.replace(/:\/\//, '://api.')
-  	return request
-	    .get({
-	      url: `${root}/users/${username}`,
-	      headers: {
-	        'User-Agent': 'request',
-	      },
-	    })
-	    .then(res => {
-	      const body = JSON.parse(res.body)
-	      let profile = body.blog || body.html_url
-
-	      // Github throwing specific errors as 200...
-	      if (!profile && body.message) {
-	        throw new Error(body.message)
-	      }
-
-	      profile = profile.startsWith('http') ? profile : `http://${profile}`
-
-	      return {
-	        login: body.login,
-	        name: body.name || username,
-	        avatar_url: body.avatar_url,
-	        profile,
-	      }
-	    })
-	},
-
-	getContributors: function(owner, name, hostname) {
-		if (!hostname) {
-			hostname = 'https://github.com';
-		}
-
-		const root = hostname.replace(/:\/\//, '://api.')
-	  const url = `${root}/repos/${owner}/${name}/contributors?per_page=100`
-	  return getContributorsPage(url)
+const getUserInfo = function(username, hostname) {
+	/* eslint-disable complexity */
+	if (!hostname) {
+		hostname = 'https://github.com';
 	}
+
+	const root = hostname.replace(/:\/\//, '://api.')
+	return request
+		.get({
+			url: `${root}/users/${username}`,
+			headers: {
+				'User-Agent': 'request',
+			},
+		})
+		.then(res => {
+			const body = JSON.parse(res.body)
+			let profile = body.blog || body.html_url
+
+			// Github throwing specific errors as 200...
+			if (!profile && body.message) {
+				throw new Error(body.message)
+			}
+
+			profile = profile.startsWith('http') ? profile : `http://${profile}`
+
+			return {
+				login: body.login,
+				name: body.name || username,
+				avatar_url: body.avatar_url,
+				profile,
+			}
+		})
+}
+
+const getContributors = function(owner, name, hostname) {
+	if (!hostname) {
+		hostname = 'https://github.com';
+	}
+
+	const root = hostname.replace(/:\/\//, '://api.')
+	const url = `${root}/repos/${owner}/${name}/contributors?per_page=100`
+	return getContributorsPage(url)
+}
+
+module.exports = {
+	getUserInfo,
+	getContributors
 }
--- src/repo/gitlab.js
+++ src/repo/gitlab.js
@@ -1,83 +1,85 @@
 const pify = require('pify')
 const request = pify(require('request'))
 
-module.exports = {
-	getUserInfo: function(username, hostname) {
-	  /* eslint-disable complexity */
-		if (!hostname) {
-			hostname = 'https://gitlab.com';
-		}
-
-	  return request
-	    .get({
-	      url: `${hostname}/api/v4/users?username=${username}`,
-	      headers: {
-	        'User-Agent': 'request',
-	      },
-	    })
-	    .then(res => {
-	      const body = JSON.parse(res.body)
-
-	      // Gitlab returns an array of users. If it is empty, it means the username provided does not exist
-	      if (!body || body.length === 0) {
-	        throw new Error(`User ${username} not found`)
-	      }
-
-				const user = body[0]
-
-	      return {
-	        login: user.username,
-	        name: user.name || username,
-	        avatar_url: user.avatar_url,
-	        profile: user.web_url.startsWith('http') ? user.web_url : `http://${user.web_url}`,
-	      }
-	    })
-	},
-
-	getContributors: function(owner, name, hostname) {
-		if (!hostname) {
-			hostname = 'https://gitlab.com';
-		}
-
-		return request
-	    .get({
-	      url: `${hostname}/api/v4/projects?search=${name}`,
-	      headers: {
-	        'User-Agent': 'request',
-	      },
-	    })
-	    .then(res => {
-	      const projects = JSON.parse(res.body)
-
-	      // Gitlab returns an array of users. If it is empty, it means the username provided does not exist
-	      if (!projects || projects.length === 0) {
-	        throw new Error(`Project ${owner}/${name} not found`)
-	      }
-
-				let project = null
-				for (let i = 0; i < projects.length; i++) {
-					if (projects[i].path_with_namespace === `${owner}/${name}`) {
-						project = projects[i]
-						break;
-					}
-				}
-
-				if (!project) {
-	        throw new Error(`Project ${owner}/${name} not found`)
-	      }
-
-				return request
-			    .get({
-			      url: `${hostname}/api/v4/projects/${project.id}/repository/contributors`,
-			      headers: {
-			        'User-Agent': 'request',
-			      },
-			    })
-			    .then(newRes => {
-			      const contributors = JSON.parse(newRes.body)
-
-			      return contributors.map(item => item.email)
-			    })
-	    })
+const getUserInfo = function(username, hostname) {
+	/* eslint-disable complexity */
+	if (!hostname) {
+		hostname = 'https://gitlab.com';
 	}
+
+	return request
+		.get({
+			url: `${hostname}/api/v4/users?username=${username}`,
+			headers: {
+				'User-Agent': 'request',
+			},
+		})
+		.then(res => {
+			const body = JSON.parse(res.body)
+
+			// Gitlab returns an array of users. If it is empty, it means the username provided does not exist
+			if (!body || body.length === 0) {
+				throw new Error(`User ${username} not found`)
+			}
+
+			const user = body[0]
+
+			return {
+				login: user.username,
+				name: user.name || username,
+				avatar_url: user.avatar_url,
+				profile: user.web_url.startsWith('http') ? user.web_url : `http://${user.web_url}`,
+			}
+		})
+}
+
+const getContributors = function(owner, name, hostname) {
+	if (!hostname) {
+		hostname = 'https://gitlab.com';
+	}
+
+	return request
+		.get({
+			url: `${hostname}/api/v4/projects?search=${name}`,
+			headers: {
+				'User-Agent': 'request',
+			},
+		})
+		.then(res => {
+			const projects = JSON.parse(res.body)
+
+			// Gitlab returns an array of users. If it is empty, it means the username provided does not exist
+			if (!projects || projects.length === 0) {
+				throw new Error(`Project ${owner}/${name} not found`)
+			}
+
+			let project = null
+			for (let i = 0; i < projects.length; i++) {
+				if (projects[i].path_with_namespace === `${owner}/${name}`) {
+					project = projects[i]
+					break;
+				}
+			}
+
+			if (!project) {
+				throw new Error(`Project ${owner}/${name} not found`)
+			}
+
+			return request
+				.get({
+					url: `${hostname}/api/v4/projects/${project.id}/repository/contributors`,
+					headers: {
+						'User-Agent': 'request',
+					},
+				})
+				.then(newRes => {
+					const contributors = JSON.parse(newRes.body)
+					return contributors.map(item => item.name)
+				})
+		})
+}
+
+module.exports = {
+	getUserInfo,
+	getContributors
 }

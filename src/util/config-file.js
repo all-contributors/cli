all-contributors/cli

commit d4dc4e262bb11aaa19b6a0cf94175262d35943b4

adds helpful error message when files is overridden or empty

src/util/config-file.js
@@ -24,6 +24,11 @@ function writeConfig(configPath, content) {
   if (!content.projectName) {
     throw new Error(`Error! Project name is not set in ${configPath}`)
   }
+  if (!content.files.length) {
+    throw new Error(
+      `Error! Project files was overridden and is empty in ${configPath}`,
+    )
+  }
   return pify(fs.writeFile)(configPath, `${JSON.stringify(content, null, 2)}\n`)
 }
 

src/util/__tests__/config-file.js
@@ -19,6 +19,15 @@ const NoNameConfigFile = {
   contributorsPerLine: 6,
   contributors: [],
 }
+const NoFilesConfigFile = {
+  projectOwner: 'jfmengels',
+  projectName: 'all-contributors-cli',
+  imageSize: 100,
+  commit: false,
+  contributorsPerLine: 6,
+  contributors: [],
+  files: [],
+}
 
 test('Reading an absent configuration file throws a helpful error', () => {
   expect(() => configFile.readConfig(absentFile)).toThrowError(
@@ -40,4 +49,12 @@ test('Should throw error and not allow editing config file if project name or ow
   expect(() =>
     configFile.writeConfig(incompleteConfigFilePath, NoNameConfigFile),
   ).toThrow(`Error! Project name is not set in ${incompleteConfigFilePath}`)
+})
+
+test(`throws if 'files' was overridden in .all-contributorsrc and is empty`, () => {
+  expect(() =>
+    configFile.writeConfig(incompleteConfigFilePath, NoFilesConfigFile),
+  ).toThrow(
+    `Error! Project files was overridden and is empty in ${incompleteConfigFilePath}`,
+  )
 })

commit 94422bdc9e1a77838a5238f50ce9d8f6199cea1e

fix: d4dc4e2-introduced error
The prior commit to this branch d4dc4e2 introduced an error
in the util/config.js file where it checks for the length of
the content.files property, whether that property exists in
the config or not.

src/util/config-file.js
@@ -24,7 +24,7 @@ function writeConfig(configPath, content) {
   if (!content.projectName) {
     throw new Error(`Error! Project name is not set in ${configPath}`)
   }
-  if (!content.files.length) {
+  if (content.files && !content.files.length) {
     throw new Error(
       `Error! Project files was overridden and is empty in ${configPath}`,
     )
a6dedd82be1815e438bc1c4c11430f875980c0d5
Berkmann18 <maxieberkmann@gmail.com>
2019-04-13 00:16:11 +0100

    feat(discover): improved the "label matcher"
    Went for a string comparison approach because RE seemed ineffective and NLP being overkill for
    matching repo labels to categories.

--- src/discover/__tests__/labelClass.js
+++ src/discover/__tests__/labelClass.js
@@ -1,9 +1,31 @@
-import classifyLabel from '../labelClass'
+import findBestCat from '../labelClass'
 
-test('simple labels', () => {
-  expect(classifyLabel('bug')).toStrictEqual('bug');
-  // expect(classifyLabel('feature')).toStrictEqual('code');
-  expect(classifyLabel('code')).toStrictEqual('code');
-  // expect(classifyLabel('test')).toStrictEqual('test');
-  // expect(classifyLabel('testing')).toStrictEqual('test');
-});
+// test('simple labels', () => {
+//   expect(classifyLabel('bug')).toStrictEqual('bug');
+//   // expect(classifyLabel('feature')).toStrictEqual('code');
+//   expect(classifyLabel('code')).toStrictEqual('code');
+//   expect(classifyLabel('test')).toStrictEqual('test');
+//   // expect(classifyLabel('testing')).toStrictEqual('test');
+// });
+
+test('exact labels', () => {
+  expect(findBestCat('bug')).toStrictEqual('bug')
+  expect(findBestCat('code')).toStrictEqual('code')
+  expect(findBestCat('test')).toStrictEqual('test')
+})
+
+test('almost the same', () => {
+  expect(findBestCat('testing')).toStrictEqual('test')
+  expect(findBestCat('sec')).toStrictEqual('security')
+})
+
+test('exceptions', () => {
+  expect(findBestCat('build')).toStrictEqual('infra')
+  expect(findBestCat('back-end')).toStrictEqual('code')
+})
+
+test('nothing found', () => {
+  expect(() => findBestCat('doing')).toThrowError(
+    'Match threshold of 0.4 not met for "doing"',
+  )
+})
--- src/discover/labelClass.js
+++ src/discover/labelClass.js
@@ -1,25 +1,50 @@
 // const ctrbType = require('../util/contributions-types');
+const strSim = require('string-similarity')
 
-const CATEGORIES = ['blog', 'bug', 'business', 'code', 'content', 'design', 'doc', 'eventOrganizing', 'example', 'financial', 'fundingFinding', 'ideas', 'infra', 'maintenance', 'platform', 'plugin', 'projectManagement', 'question', 'review', 'security', 'talk', 'test', 'tool', 'translation', 'tutorial', 'userTesting', 'video'];//Object.keys(ctrbType('github'));
+const CATEGORIES = [
+  'blog',
+  'bug',
+  'business',
+  'code',
+  'content',
+  'design',
+  'doc',
+  'eventOrganizing',
+  'example',
+  'financial',
+  'fundingFinding',
+  'ideas',
+  'infra',
+  'maintenance',
+  'platform',
+  'plugin',
+  'projectManagement',
+  'question',
+  'review',
+  'security',
+  'talk',
+  'test',
+  'tool',
+  'translation',
+  'tutorial',
+  'userTesting',
+  'video',
+] //Object.keys(ctrbType('github'));
 /*
 ['blog', 'bug', 'business', 'code', 'content', 'design', 'doc', 'eventOrganizing', 'example', 'financial', 'fundingFinding', 'ideas', 'infra', 'maintenance', 'platform', 'plugin', 'projectManagement', 'question', 'review', 'security', 'talk', 'test', 'tool', 'translation', 'tutorial', 'userTesting', 'video']
 */
 
-const labelPrefixes = [
-  'type:',
-  'cat:',
-  'type-',
-  'cat-',
-];
+/*const labelPrefixes = ['type:', 'cat:', 'type-', 'cat-']
 
-const RE = /(type|cat|category)?\s*[:-]?\s*(\w+)/gi;
+const RE = /(type|cat|category)?\s*[:-]?\s*(\w+)/gi
 
-module.exports = function classifyLabel(label) {
-  const match = RE.exec(label);
-  console.log('match=', match);
-  if (match === null) throw new Error(`The label ${label} couldn't be classified`);
-  const extractedLabel = match[match.length - 1];
-  if (CATEGORIES.includes(extractedLabel)) return extractedLabel;
+function classifyLabel(label) {
+  const match = RE.exec(label)
+  console.log('match=', match)
+  if (match === null)
+    throw new Error(`The label ${label} couldn't be classified`)
+  const extractedLabel = match[match.length - 1]
+  if (CATEGORIES.includes(extractedLabel)) return extractedLabel
   else {
     switch (extractedLabel) {
       case 'graphic':
@@ -42,7 +67,34 @@ module.exports = function classifyLabel(label) {
       case 'testing':
         return 'test'
       default:
-        throw new Error(`The label ${extractedLabel} couldn't be associated to a category from https://allcontributors.org/docs/en/emoji-key`);
+        throw new Error(
+          `The label ${extractedLabel} couldn't be associated to a category from https://allcontributors.org/docs/en/emoji-key`,
+        )
     }
   }
+}*/
+
+const MATCH_THRESHOLD = 0.4 //40% to allow for shorter/longer versions of categories
+const SIM_EXCEPTIONS = {
+  //Those are matched wrongly
+  graphic: 'design',
+  'front-end': 'code',
+  'back-end': 'code',
+  ux: 'design',
+  ui: 'design', //or code
+  ci: 'infra',
+  cd: 'infra',
+  build: 'infra',
 }
+
+function findBestCat(label) {
+  const lbl = label.toLowerCase()
+  if (lbl in SIM_EXCEPTIONS) return SIM_EXCEPTIONS[lbl]
+  const match = strSim.findBestMatch(lbl, CATEGORIES)
+  if (match.bestMatch.rating >= MATCH_THRESHOLD) return match.bestMatch.target
+  throw new Error(
+    `Match threshold of ${MATCH_THRESHOLD} not met for "${label}"`,
+  )
+}
+
+module.exports = findBestCat

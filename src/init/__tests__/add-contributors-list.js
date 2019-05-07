febecff496aa8d6cbbae3dedf622c45a44696a38
Maximilian Berkmann <maxieberkmann@gmail.com>
2019-05-07 16:48:58 +0100

    feat(init): creates the contributor file if not found (#158)
    * feat(init): creates the contributor file if not found
    
    If not found, the specified contributor file (defaulting to `README.md`) will be created prior to
    injections in it.
    
    re https://github.com/all-contributors/all-contributors-bot/issues/105
    
    * refactor(init): renamed a module and its export
    
    Renamed `check-file` to `file-exist` and its exported function to `ensureFileExists`
    
    * test(init): added contributors file test
    
    Added two test cases for the contributors file existence check

--- src/init/__tests__/add-contributors-list.js
+++ src/init/__tests__/add-contributors-list.js
@@ -1,4 +1,6 @@
+import {unlink} from 'fs'
 import {addContributorsList} from '../init-content'
+import ensureFileExists from '../file-exist'
 
 test('insert list under contributors section', () => {
   const content = [
@@ -26,4 +28,22 @@ test('create contributors section if content is empty', () => {
   const result = addContributorsList(content)
 
   expect(result).toMatchSnapshot()
+})
+
+test('README exists', done => {
+  const file = 'README.md'
+  ensureFileExists(file)
+    .then(data => expect(data).toStrictEqual(file))
+    .then(_ => done())
+})
+
+test("LOREM doesn't exists", done => {
+  const file = 'LOREM.md'
+  ensureFileExists(file).then(data => {
+    expect(data).toStrictEqual(file)
+    return unlink(file, err => {
+      if (err) throw err
+      done()
+    })
+  })
 })

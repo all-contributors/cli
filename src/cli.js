6cbec25c9fe3a99cc720cde91f777f0d33e55bcb
Fabrizio Destro <7031675+dexpota@users.noreply.github.com>
2019-10-17 18:02:39 +0200

    Fix issue #209

--- src/cli.js
+++ src/cli.js
@@ -72,7 +72,7 @@ function startGeneration(argv) {
 }
 
 function addContribution(argv) {
-  const username = String(argv._[1])
+  const username = argv._[1] !== undefined ? String(argv._[1]) : undefined
   const contributions = argv._[2]
   // Add or update contributor in the config file
   return updateContributors(argv, username, contributions).then(data => {

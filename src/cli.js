6c22d442e6d1026db08a4748c2bb381864082cb9
jamesgeorge007 <jamesgeorge998001@gmail.com>
2019-11-26 16:52:11 +0530

    chore: make use of inhouse command recommendation

--- src/cli.js
+++ src/cli.js
@@ -5,10 +5,6 @@ const path = require('path')
 const yargs = require('yargs')
 const chalk = require('chalk')
 const inquirer = require('inquirer')
-const didYouMean = require('didyoumean')
-
-// Setting edit length to be 60% of the input string's length
-didYouMean.threshold = 0.6
 
 const init = require('./init')
 const generate = require('./generate')
@@ -24,6 +20,7 @@ const yargv = yargs
   .alias('h', 'help')
   .alias('v', 'version')
   .version()
+  .recommendCommands()
   .command('generate', 'Generate the list of contributors')
   .usage('Usage: $0 generate')
   .command('add', 'add a new contributor')
@@ -49,15 +46,6 @@ const yargv = yargs
       }
     }
   }).argv
-
-function suggestCommands(cmd) {
-  const availableCommands = ['generate', 'add', 'init', 'check']
-  const suggestion = didYouMean(cmd, availableCommands)
-
-  if (suggestion) {
-    console.log(chalk.bold(`Did you mean ${suggestion}`))
-  }
-}
 
 function startGeneration(argv) {
   return Promise.all(
@@ -183,7 +171,6 @@ promptForCommand(yargv)
       case 'check':
         return checkContributors(yargv)
       default:
-        suggestCommands(command)
         throw new Error(`Unknown command ${command}`)
     }
   })

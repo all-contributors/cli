e8788f7ea8b4d883cf05181013d9b3f85d3c677d
Justin Dalrymple <justin.dalrymple@dialogue.co>
2026-01-24 17:43:49 -0500

    test: add missing test

--- src/init/__tests__/add-contributors-list.js
+++ src/init/__tests__/add-contributors-list.js
@@ -17,6 +17,13 @@ describe('addContributorsList', () => {
     expect(result).toMatchSnapshot()
   })
 
+  test('create contributors section if it is absent', () => {
+    const content = ['# project', '', 'Description'].join('\n')
+    const result = addContributorsList(content)
+
+    expect(result).toMatchSnapshot()
+  })
+
   test('create contributors section if content is empty', () => {
     const content = ''
     const result = addContributorsList(content)

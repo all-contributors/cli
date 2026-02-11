0f82075cc89be383951a404a132c767c0a491427
Leah Wasser <leah@pyopensci.org>
2026-02-10 19:31:33 -0700

    chore: migrate build/ babel to native configuration
    Apply suggestion from @JoshuaKGoldberg
    
    Co-authored-by: Josh Goldberg ✨ <git@joshuakgoldberg.com>
    
    chore: update config settings and review comments

---
+++ babel.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  ignore: [
+    '**/__snapshots__/**',
+    '**/__tests__/**',
+    '**/fixtures/**',
+    '__tests__/**',
+  ],
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {node: '22.22.0'},
+        modules: 'commonjs',
+      },
+    ],
+  ],
+  plugins: ['@babel/plugin-transform-runtime'],
+}

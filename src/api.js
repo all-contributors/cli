all-contributors/cli

commit 7d8c2bffd04d732fb4ab899bb8a1376a0c206a6d

refactor: Update calls to prettier functions to account for latest async API  (#403)
* Should fix tests

# Conflicts:
#	package.json
#	src/init/__tests__/__snapshots__/add-contributors-list.js.snap

* Updating spacing

src/cli.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-/* eslint-disable no-console */
 
 const path = require('path')
 const yargs = require('yargs')
@@ -15,39 +14,47 @@ const updateContributors = require('./contributors')
 const cwd = process.cwd()
 const defaultRCFile = path.join(cwd, '.all-contributorsrc')
 
-const yargv = yargs
-  .scriptName('all-contributors')
-  .help('help')
-  .alias('h', 'help')
-  .alias('v', 'version')
-  .version()
-  .recommendCommands()
-  .command('generate', `Generate the list of contributors\n\nUSAGE: all-contributors generate`)
-  .command('add', `Add a new contributor\n\nUSAGE: all-contributors add <username> <comma-separated contributions>`)
-  .command('init', `Prepare the project to be used with this tool\n\nUSAGE: all-contributors init`)
-  .command(
-    'check',
-    `Compare contributors from the repository with the ones credited in .all-contributorsrc'\n\nUSAGE: all-contributors check`)
-  .boolean('commit')
-  .default('files', ['README.md'])
-  .default('contributorsPerLine', 7)
-  .option('contributorsSortAlphabetically', {
-    type: 'boolean',
-    default: false,
-    description:
-      'Sort the list of contributors alphabetically in the generated list',
-  })
-  .default('contributors', [])
-  .default('config', defaultRCFile)
-  .config('config', configPath => {
-    try {
-      return util.configFile.readConfig(configPath)
-    } catch (error) {
-      if (error instanceof SyntaxError || configPath !== defaultRCFile) {
-        onError(error)
-      }
-    }
-  }).argv
+function getArgs() {
+  return yargs
+    .scriptName('all-contributors')
+    .option('config', {
+      alias: 'c',
+      type: 'string',
+      default: defaultRCFile,
+      description: 'Path to config file',
+    })
+    .option('contributorsSortAlphabetically', {
+      type: 'boolean',
+      default: false,
+      description:
+        'Sort the list of contributors alphabetically in the generated list',
+    })
+    .help('help')
+    .alias('h', 'help')
+    .alias('v', 'version')
+    .version()
+    .recommendCommands()
+    .command(
+      'generate',
+      `Generate the list of contributors\n\nUSAGE: all-contributors generate`,
+    )
+    .command(
+      'add',
+      `Add a new contributor\n\nUSAGE: all-contributors add <username> <comma-separated contributions>`,
+    )
+    .command(
+      'init',
+      `Prepare the project to be used with this tool\n\nUSAGE: all-contributors init`,
+    )
+    .command(
+      'check',
+      `Compare contributors from the repository with the ones credited in .all-contributorsrc'\n\nUSAGE: all-contributors check`,
+    )
+    .boolean('commit')
+    .default('files', ['README.md'])
+    .default('contributorsPerLine', 7)
+    .default('contributors', []).argv
+}
 
 function startGeneration(argv) {
   return Promise.all(
@@ -61,23 +68,28 @@ function startGeneration(argv) {
   )
 }
 
-function addContribution(argv) {
-  util.configFile.readConfig(argv.config) // ensure the config file exists
+async function addContribution(argv) {
+  // ensure the config file exists
+  await util.configFile.readConfig(argv.config)
+
   const username = argv._[1] === undefined ? undefined : String(argv._[1])
   const contributions = argv._[2]
+
   // Add or update contributor in the config file
-  return updateContributors(argv, username, contributions).then(data => {
-    argv.contributors = data.contributors
-    return startGeneration(argv).then(() => {
-      if (argv.commit) {
-        return util.git.commit(argv, data)
-      }
-    })
-  })
+  const data = updateContributors(argv, username, contributions)
+
+  argv.contributors = data.contributors
+
+  await startGeneration(argv)
+
+  // Commit if configured
+  if (argv.commit) {
+    return util.git.commit(argv, data)
+  }
 }
 
-function checkContributors(argv) {
-  const configData = util.configFile.readConfig(argv.config)
+async function checkContributors(argv) {
+  const configData = await util.configFile.readConfig(argv.config)
 
   return repo
     .getContributors(
@@ -123,14 +135,6 @@ function checkContributors(argv) {
     })
 }
 
-function onError(error) {
-  if (error) {
-    console.error(error.stack || error.message || error)
-    process.exit(1)
-  }
-  process.exit(0)
-}
-
 function promptForCommand(argv) {
   const questions = [
     {
@@ -162,19 +166,28 @@ function promptForCommand(argv) {
   })
 }
 
-promptForCommand(yargv)
-  .then(command => {
+async function run() {
+  try {
+    const argv = getArgs()
+    const command = await promptForCommand(argv)
+
     switch (command) {
       case 'init':
         return init()
       case 'generate':
-        return startGeneration(yargv)
+        return startGeneration(argv)
       case 'add':
-        return addContribution(yargv)
+        return addContribution(argv)
       case 'check':
-        return checkContributors(yargv)
+        return checkContributors(argv)
       default:
         throw new Error(`Unknown command ${command}`)
     }
-  })
-  .catch(onError)
+  } catch (e) {
+    /* eslint-disable-next-line no-console */
+    console.error(e.stack || e.message || e)
+    process.exit(1)
+  }
+}
+
+run()

src/util/git.js
@@ -53,12 +53,12 @@ const spawnGitCommand = pify((args, cb) => {
   })
 })
 
-function commit(options, data) {
+async function commit(options, data) {
   const files = options.files.concat(options.config)
   const absolutePathFiles = files.map(file => {
     return path.resolve(process.cwd(), file)
   })
-  const config = readConfig(options.config)
+  const config = await readConfig(options.config)
   const commitConvention = conventions[config.commitConvention]
 
   return spawnGitCommand(['add'].concat(absolutePathFiles)).then(() => {

src/util/__tests__/config-file.js
@@ -1,7 +1,7 @@
-import configFile from '../config-file'
+import {writeConfig, readConfig, writeContributors} from '../config-file'
 
 const absentFile = './abc'
-const absentConfileFileExpected = `Configuration file not found: ${absentFile}`
+const absentConfigFileExpected = `Configuration file not found: ${absentFile}`
 const incompleteConfigFilePath = './.all-contributorsrc'
 const NoOwnerConfigFile = {
   projectOwner: '',
@@ -29,32 +29,36 @@ const NoFilesConfigFile = {
   files: [],
 }
 
-test('Reading an absent configuration file throws a helpful error', () => {
-  expect(() => configFile.readConfig(absentFile)).toThrowError(
-    absentConfileFileExpected,
+test('Reading an absent configuration file throws a helpful error', async () => {
+  await expect(readConfig(absentFile)).rejects.toThrowError(
+    absentConfigFileExpected,
   )
 })
 
 test('Writing contributors in an absent configuration file throws a helpful error', async () => {
-  const resolvedError = await configFile
-    .writeContributors(absentFile, [])
-    .catch(e => e)
-  expect(resolvedError.message).toBe(absentConfileFileExpected)
+  await expect(writeContributors(absentFile, [])).rejects.toThrow(
+    absentConfigFileExpected,
+  )
 })
 
-test('Should throw error and not allow editing config file if project name or owner is not set', () => {
-  expect(() =>
-    configFile.writeConfig(incompleteConfigFilePath, NoOwnerConfigFile),
-  ).toThrow(`Error! Project owner is not set in ${incompleteConfigFilePath}`)
-  expect(() =>
-    configFile.writeConfig(incompleteConfigFilePath, NoNameConfigFile),
-  ).toThrow(`Error! Project name is not set in ${incompleteConfigFilePath}`)
+test('Should throw error and not allow editing config file if project name or owner is not set', async () => {
+  await expect(
+    writeConfig(incompleteConfigFilePath, NoOwnerConfigFile),
+  ).rejects.toThrow(
+    `Error! Project owner is not set in ${incompleteConfigFilePath}`,
+  )
+
+  await expect(
+    writeConfig(incompleteConfigFilePath, NoNameConfigFile),
+  ).rejects.toThrow(
+    `Error! Project name is not set in ${incompleteConfigFilePath}`,
+  )
 })
 
-test(`throws if 'files' was overridden in .all-contributorsrc and is empty`, () => {
-  expect(() =>
-    configFile.writeConfig(incompleteConfigFilePath, NoFilesConfigFile),
-  ).toThrow(
+test(`throws if 'files' was overridden in .all-contributorsrc and is empty`, async () => {
+  await expect(
+    writeConfig(incompleteConfigFilePath, NoFilesConfigFile),
+  ).rejects.toThrow(
     `Error! Project files was overridden and is empty in ${incompleteConfigFilePath}`,
   )
 })

src/util/__tests__/formatting.js
@@ -1,5 +1,5 @@
 import path from 'path'
-import formatting from '../formatting'
+import {formatConfig} from '../formatting'
 
 const content = {contributors: [{id: 'abc123'}]}
 
@@ -19,17 +19,16 @@ const presentConfigFileExpected = `{
 			"id": "abc123"
 		}
 	]
-}
-`
+}`
 
-test('falls back to JSON.stringify when the configPath cannot resolve to a config', () => {
-  expect(formatting.formatConfig(absentFile, content)).toBe(
-    absentConfigFileExpected,
-  )
+test('falls back to JSON.stringify when the configPath cannot resolve to a config', async () => {
+  const output = await formatConfig(absentFile, content)
+
+  expect(output).toBe(absentConfigFileExpected)
 })
 
-test('uses Prettier when the configPath can resolve to a config', () => {
-  expect(formatting.formatConfig(presentFile, content)).toBe(
-    presentConfigFileExpected,
-  )
+test('uses Prettier when the configPath can resolve to a config', async () => {
+  const output = await formatConfig(presentFile, content)
+
+  expect(output).toBe(presentConfigFileExpected)
 })

src/util/config-file.js
@@ -1,22 +1,27 @@
-const fs = require('fs')
-const pify = require('pify')
-const _ = require('lodash/fp')
-const jf = require('json-fixer')
-const {formatConfig} = require('./formatting')
+import {readFile, writeFile} from 'fs/promises'
+import jf from 'json-fixer'
+import _ from 'lodash/fp'
+import {formatConfig} from './formatting'
 
-function readConfig(configPath) {
+export async function readConfig(configPath) {
   try {
-    const {data: config, changed} = jf(fs.readFileSync(configPath, 'utf-8'))
+    const configFileContents = await readFile(configPath, 'utf-8')
+    const {data: config, changed} = jf(configFileContents)
+
     if (!('repoType' in config)) {
       config.repoType = 'github'
     }
+
     if (!('commitConvention' in config)) {
       config.commitConvention = 'angular'
     }
+
     if (changed) {
+      const formatterConfig = await formatConfig(configPath, config)
       //Updates the file with fixes
-      fs.writeFileSync(configPath, formatConfig(configPath, config))
+      await writeFile(configPath, formatterConfig)
     }
+
     return config
   } catch (error) {
     if (error instanceof SyntaxError) {
@@ -24,17 +29,20 @@ function readConfig(configPath) {
         `Configuration file has malformed JSON: ${configPath}. Error:: ${error.message}`,
       )
     }
+
     if (error.code === 'ENOENT') {
       throw new Error(`Configuration file not found: ${configPath}`)
     }
+
     throw error
   }
 }
 
-function writeConfig(configPath, content) {
+export async function writeConfig(configPath, content) {
   if (!content.projectOwner) {
     throw new Error(`Error! Project owner is not set in ${configPath}`)
   }
+
   if (!content.projectName) {
     throw new Error(`Error! Project name is not set in ${configPath}`)
   }
@@ -44,25 +52,19 @@ function writeConfig(configPath, content) {
       `Error! Project files was overridden and is empty in ${configPath}`,
     )
   }
-  return pify(fs.writeFile)(
-    configPath,
-    `${formatConfig(configPath, content)}\n`,
-  )
+
+  return writeFile(configPath, `${await formatConfig(configPath, content)}\n`)
 }
 
-function writeContributors(configPath, contributors) {
+export async function writeContributors(configPath, contributors) {
   let config
+
   try {
-    config = readConfig(configPath)
+    config = await readConfig(configPath)
   } catch (error) {
     return Promise.reject(error)
   }
   const content = _.assign(config, {contributors})
-  return writeConfig(configPath, content)
-}
 
-module.exports = {
-  readConfig,
-  writeConfig,
-  writeContributors,
+  return writeConfig(configPath, content)
 }

src/util/formatting.js
@@ -1,21 +1,24 @@
-function formatConfig(configPath, content) {
+import prettier from 'prettier'
+
+export async function formatConfig(configPath, content) {
   const stringified = JSON.stringify(content, null, 2)
+
   try {
-    const prettier = require('prettier')
-    const prettierConfig = prettier.resolveConfig.sync(configPath, {
+    const prettierConfig = await prettier.resolveConfig(configPath, {
       useCache: false,
     })
 
-    return prettierConfig
-      ? prettier.format(stringified, {...prettierConfig, parser: 'json'})
-      : stringified
+    if (!prettierConfig) return stringified
+
+    const formattedOutput = await prettier.format(stringified, {
+      ...prettierConfig,
+      parser: 'json',
+    })
+
+    return formattedOutput.trimEnd()
   } catch (error) {
     // If Prettier can't be required or throws in general,
     // assume it's not usable and we should fall back to JSON.stringify
     return stringified
   }
-}
-
-module.exports = {
-  formatConfig,
 }

commit 797e16763e3325cb684c3f659e11c829a5cf1419

fix: Fixing issues with commands introduced in #403 (#440)
* Fixing issues with commands introduced in commit  7d8c2bffd

Also replacing chalk with something lighter weight

* Linting

src/api.js
@@ -3,15 +3,15 @@
 // This is to support adding contributors using the AllContributors GitHub Bot (see github.com/all-contributors/all-contributors-bot
 // These Node API's are intended to be network and side effect free, everything should be in memory with no io to network/disk
 
-const chalk = require('chalk')
+const YoctoColors = require('yoctocolors')
 
 const addContributorWithDetails = require('./contributors/addWithDetails')
 const generate = require('./generate')
 const {addContributorsList, addBadge} = require('./init/init-content')
 
 process.stdout.write(
-  chalk.yellow(
-    `${chalk.bold(
+  YoctoColors.yellow(
+    `${YoctoColors.bold(
       'WARNING',
     )} :: Using the all-contributors node-api comes with zero guarantees of stability and may contain breaking changes without warning\n`,
   ),

src/cli.js
@@ -2,8 +2,8 @@
 
 const path = require('path')
 const yargs = require('yargs/yargs')
+const YoctoColors = require('yoctocolors')
 const {hideBin} = require('yargs/helpers')
-const chalk = require('chalk')
 const inquirer = require('inquirer')
 
 const init = require('./init')
@@ -70,14 +70,11 @@ function startGeneration(argv) {
 }
 
 async function addContribution(argv) {
-  // ensure the config file exists
-  await util.configFile.readConfig(argv.config)
-
   const username = argv._[1] === undefined ? undefined : String(argv._[1])
   const contributions = argv._[2]
 
   // Add or update contributor in the config file
-  const data = updateContributors(argv, username, contributions)
+  const data = await updateContributors(argv, username, contributions)
 
   argv.contributors = data.contributors
 
@@ -90,22 +87,20 @@ async function addContribution(argv) {
 }
 
 async function checkContributors(argv) {
-  const configData = await util.configFile.readConfig(argv.config)
-
   return repo
     .getContributors(
-      configData.projectOwner,
-      configData.projectName,
-      configData.repoType,
-      configData.repoHost,
+      argv.projectOwner,
+      argv.projectName,
+      argv.repoType,
+      argv.repoHost,
     )
     .then(repoContributors => {
-      const checkKey = repo.getCheckKey(configData.repoType)
-      const knownContributions = configData.contributors.reduce((obj, item) => {
+      const checkKey = repo.getCheckKey(argv.repoType)
+      const knownContributions = argv.contributors.reduce((obj, item) => {
         obj[item[checkKey]] = item.contributions
         return obj
       }, {})
-      const knownContributors = configData.contributors.map(
+      const knownContributors = argv.contributors.map(
         contributor => contributor[checkKey],
       )
 
@@ -122,14 +117,17 @@ async function checkContributors(argv) {
 
       if (missingInConfig.length) {
         process.stdout.write(
-          chalk.bold('Missing contributors in .all-contributorsrc:\n'),
+          YoctoColors.bold('Missing contributors in .all-contributorsrc:\n'),
         )
-        process.stdout.write(`    ${missingInConfig.join(', ')}\n`)
+        process.stdout.write(`${missingInConfig.join(', ')}\n`)
       }
 
       if (missingFromRepo.length) {
+        process.stdout.write('\n')
         process.stdout.write(
-          chalk.bold('Unknown contributors found in .all-contributorsrc:\n'),
+          YoctoColors.bold(
+            'Unknown contributors found in .all-contributorsrc:\n',
+          ),
         )
         process.stdout.write(`${missingFromRepo.join(', ')}\n`)
       }
@@ -169,6 +167,18 @@ function promptForCommand(argv) {
 async function run() {
   try {
     const argv = getArgs()
+
+    // Load and merge config file data into argv
+    try {
+      const configData = await util.configFile.readConfig(argv.config)
+      Object.assign(argv, configData)
+    } catch (error) {
+      if (error instanceof SyntaxError || argv.config !== defaultRCFile) {
+        throw error
+      }
+      // If default config file doesn't exist, that's okay
+    }
+
     const command = await promptForCommand(argv)
 
     switch (command) {

src/util/git.js
@@ -3,7 +3,6 @@ const spawn = require('child_process').spawn
 const _ = require('lodash/fp')
 const pify = require('pify')
 const conventions = require('../init/commit-conventions')
-const {readConfig} = require('./config-file')
 
 const commitTemplate =
   '<%= prefix %> <%= (newContributor ? "Add" : "Update") %> @<%= username %> as a contributor'
@@ -58,8 +57,7 @@ async function commit(options, data) {
   const absolutePathFiles = files.map(file => {
     return path.resolve(process.cwd(), file)
   })
-  const config = await readConfig(options.config)
-  const commitConvention = conventions[config.commitConvention]
+  const commitConvention = conventions[options.commitConvention]
 
   return spawnGitCommand(['add'].concat(absolutePathFiles)).then(() => {
     let commitMessage = _.template(options.commitTemplate || commitTemplate)({